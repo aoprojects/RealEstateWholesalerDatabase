6116f29c8b40afe178c03d09f9f09fc40cafb4cf
Alison Ostendorf <aostendorf@gmail.com>
2015-10-19 16:00:16 -0400

    added all wholesaler user fields without saving ability

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151018224158) do
+ActiveRecord::Schema.define(version: 20151019194424) do
 
   create_table "listings", force: :cascade do |t|
     t.string   "name"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 20151018224158) do
     t.string   "phone"
     t.text     "details"
     t.string   "company"
+    t.string   "url"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
