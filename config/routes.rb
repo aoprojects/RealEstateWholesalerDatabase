6a7fcc31fc72de1394d39341b690241381452d5e
Alison Ostendorf <aostendorf@gmail.com>
2016-01-19 23:10:05 -0500

    add advert_summary_path and routes and link in my account

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
   get "newsletter" => "pages#newsletter"
   get "new_listing_choice" => "pages#new_listing_choice"
   get "privacy_policy" => "pages#privacy_policy"
+  get "advert_summary" => "pages#advert_summary"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
