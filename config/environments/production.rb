b05e0eeea201213fc11ad546af72dc27207ac06f
Alison Ostendorf <aostendorf@gmail.com>
2016-01-25 21:56:05 -0500

    add sendgrid production configurations

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,5 +79,17 @@ Rails.application.configure do
 
   # for devise
   config.action_mailer.default_url_options = { host: 'https://rewholesalerdb.herokuapp.com/' }
+  config.action_mailer.delivery_method = :smtp
+
+      ActionMailer::Base.smtp_settings = {
+      :address        => 'smtp.sendgrid.net',
+      :port           => '587',
+      :authentication => :plain,
+      :user_name      => ENV['SENDGRID_USERNAME'],
+      :password       => ENV['SENDGRID_PASSWORD'],
+      :domain         => 'heroku.com',
+      :enable_starttls_auto => true
+      }
+
 
 end
