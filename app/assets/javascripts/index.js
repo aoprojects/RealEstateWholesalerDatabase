9ce16a2944eb83736199cd009d31f605e6c58dce
Alison Ostendorf <aostendorf@gmail.com>
2016-01-26 09:58:11 -0500

    add jquery attempt to drop down faq privacy policy

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,2 +1,20 @@
 
 // Begin JQuery code
+$(document).ready(function(){
+
+
+$('.question').click(function() {
+	$(this).next().fadeToggle("fast");
+	$(this).children(".glyphicon").toggleClass(".collapse");
+});
+
+
+
+
+
+
+
+
+
+
+)};
