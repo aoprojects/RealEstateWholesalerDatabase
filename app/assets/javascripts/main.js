abffc11660969cb6fe31eb47799b2ebd45fb78b0
Alison Ostendorf <aostendorf@gmail.com>
2016-05-17 14:12:39 -0400

    remove redundant js links in main application.html.erb and move js code to main.js file

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,12 +1,4 @@
 $(document).ready(function() {
-	$('.question').click(function() {
-		console.log("hello");
-		$(this).next().fadeToggle("fast");
-		$(this).children(".glyphicon").toggleClass("collapse");
-
-	});
-
-
 	$("#hamburger-menu-area").click(function() {
 		$("#nav-options-dropped").toggle("fast");
 	});
