eb42068b3a603f6a91fc91a0957ae41d8fbb4d7f
Alison Ostendorf <aostendorf@gmail.com>
2016-06-01 16:09:14 -0400

    add limit 50 to amount of listings a user can create

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -5,6 +5,14 @@ class Listing < ActiveRecord::Base
 
 	belongs_to :user
 
+	validate :listing_count_within_limit, :on => :create
+
+  def listing_count_within_limit
+    if self.user.listings(:reload).count >= 50
+      errors.add(:base, "To create more than 50 listings, please sign out and create another user account.")
+    end
+  end
+
   def city=(val)
     write_attribute(:city, val.titleize)
   end
