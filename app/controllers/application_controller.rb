9121b3afb2129b70351c84348f797f3c82814521
Alison Ostendorf <aostendorf@gmail.com>
2016-06-01 13:53:54 -0400

    terms of service acceptance working. Added :terms to permit for create action in user model

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,8 +10,8 @@ class ApplicationController < ActionController::Base
 
     def configure_permitted_parameters
          devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:email, :password) }
-         devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:email, :password, :password_confirmation, :company, :email, :phone, :url, :specialties, :details) }
-         devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:company, :email, :phone, :url, :specialties, :details, :password, :password_confirmation, :current_password, :terms) }
+         devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:email, :password, :password_confirmation, :company, :email, :phone, :url, :specialties, :details, :terms) }
+         devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:company, :email, :phone, :url, :specialties, :details, :password, :password_confirmation, :current_password) }
     end
 
 end
