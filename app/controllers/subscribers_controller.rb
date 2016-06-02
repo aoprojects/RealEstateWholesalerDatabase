33d2671adf976d028a94fbec5a1239c0dddfe4c5
Alison Ostendorf <aostendorf@gmail.com>
2016-06-02 19:23:30 -0400

    add info to create method, and create plan and account on stripe.com

--- app/controllers/subscribers_controller.rb
+++ app/controllers/subscribers_controller.rb
@@ -4,7 +4,21 @@ class SubscribersController < ApplicationController
 	end
 
 	def update
-		
+		# add in token from JS that the user submits. This is the credit card.
+		token = params[:stripeToken]
+
+		customer = Stripe::Customer.create(
+			card: token,
+			# plan is subscription plan that we want to enroll customer in, as we called in in stripe account
+			plan: 1001
+			email: current_user.email
+			)
+
+		current_user.subscribed = true
+		current_user.stripeid = customer.id
+		current_user.save
+
+		redirect_to new_listing_path
 	end
 
 end
