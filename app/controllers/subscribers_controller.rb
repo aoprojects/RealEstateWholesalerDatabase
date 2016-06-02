42f17b14e8b1df4bb2bb6d4b0c4ccd6c98f36a60
Alison Ostendorf <aostendorf@gmail.com>
2016-06-02 18:13:53 -0400

    add subscribed and stripeid to users table, and create subscribers controller

---
+++ app/controllers/subscribers_controller.rb
@@ -0,0 +1,10 @@
+class SubscribersController < ApplicationController
+	def new
+		# stripe gem takes care of all things needed in here
+	end
+
+	def update
+		
+	end
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160427021603) do
+ActiveRecord::Schema.define(version: 20160602221043) do
 
   create_table "listings", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -52,6 +52,8 @@ ActiveRecord::Schema.define(version: 20160427021603) do
     t.text     "details"
     t.string   "company"
     t.string   "url"
+    t.boolean  "subscribed"
+    t.string   "stripeid"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
