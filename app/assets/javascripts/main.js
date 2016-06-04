adbc2c773ea4ff7f9eb5ea7a53f2a8664c2231d9
Alison Ostendorf <aostendorf@gmail.com>
2016-06-03 22:35:02 -0400

    add yellow background to #notice via css

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -4,4 +4,10 @@ $(document).ready(function() {
 		event.stopImmediatePropagation();
 		$("#nav-options-dropped").toggle("fast");
 	});
+
+	// todo we want the paragraph yellow area only to display for a notice if there is a notice
+	if $('#notice').innerHTML == nil {
+		$('p#notice').css('display:none');
+	};
+
 });
