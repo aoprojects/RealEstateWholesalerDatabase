946c31194fd3d5fc70c2bc456db645d43ea91bf0
Alison Ostendorf <aostendorf@gmail.com>
2016-06-06 17:19:27 -0400

    add favicon and begin creating toggle ability for privacy page

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -5,6 +5,13 @@ $(document).ready(function() {
 		$("#nav-options-dropped").toggle("fast");
 	});
 
+	$(".question").click(function(event) {
+		event.stopImmediatePropagation();
+		$(this).next().toggle("fast");
+		$(this).child(".will_toggle_after").toggle("fast");
+		$(this).child(".will_toggle_before").toggle("fast");
+	});
+
 	// todo we want the paragraph yellow area only to display for a notice if there is a notice
 	$("p").each(function(){
   		if ($.trim($(this).text()) == ""){
