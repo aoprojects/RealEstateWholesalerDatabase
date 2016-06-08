00ab645c98576ec656ae07605a06e4972a80fe01
Alison Ostendorf <aostendorf@gmail.com>
2016-06-08 19:30:37 -0400

    change migration file to add subscribed boolean to users, make default false, re-migrate up

--- db/schema.rb
+++ db/schema.rb
@@ -35,24 +35,24 @@ ActiveRecord::Schema.define(version: 20160602221043) do
   add_index "resources", ["user_id"], name: "index_resources_on_user_id"
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.string   "email",                  default: "",    null: false
+    t.string   "encrypted_password",     default: "",    null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",          default: 0,     null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
+    t.datetime "created_at",                             null: false
+    t.datetime "updated_at",                             null: false
     t.string   "specialties"
     t.string   "phone"
     t.text     "details"
     t.string   "company"
     t.string   "url"
-    t.boolean  "subscribed"
+    t.boolean  "subscribed",             default: false
     t.string   "stripeid"
   end
 
