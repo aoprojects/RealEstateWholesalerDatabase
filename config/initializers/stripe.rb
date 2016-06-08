fc754263fd72ff9b90f52e0fe511602862bdff11
Alison Ostendorf <aostendorf@gmail.com>
2016-06-08 15:25:29 -0400

    add application.yml file and put 2 keys for stripe test into that, in desktop

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -2,5 +2,5 @@ Rails.configuration.stripe = {
   :publishable_key => ENV['STRIPE_PUBLISHABLE_KEY'],
   :secret_key      => ENV['STRIPE_SECRET_KEY']
 }
-
+# these keys should be coded in the gitignored file of application.yml
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
