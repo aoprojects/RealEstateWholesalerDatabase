f327fceada701cccde3bb64a35c5adb68a5ac993
Alison Ostendorf <aostendorf@gmail.com>
2016-06-29 15:48:50 -0400

    add length limit to phone number field of 18 char

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ActiveRecord::Base
   # TODO validate presence of non email and password fields, on create of new listing, not new user
 
   validates :email, presence: true
-  validates :phone, presence: true
+  validates :phone, presence: true, :length => { :maximum => 18 }
   validates :url, presence: true
   validates :company, presence: true, :length => { :maximum => 40 }
   validates :details, presence: true, :length => { :maximum => 500 }
